c92794089a1f76163d4de0636016748e17134a45
c3eyfon <162065693+c3eyfon@users.noreply.github.com>
2025-11-21 11:04:54 +1100

    update1

--- p2.2/app.js
+++ p2.2/app.js
@@ -1,2 +1,8 @@
 let myMovie = {Title: "The Longest day", Year: 1964};
 console.log(myMovie);
+
+myMovie.rating = 5;
+myMovie.Summary = "Summary: World War II movie about the Normandy landings";
+
+console.log(myMovie);
+
